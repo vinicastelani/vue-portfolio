ddf2b0d8b1ed29c8f96696fda4e8383f95a91396
vinicius castelani <ext.vinicius.santos.vcs@aurenenergia.com.br>
2023-03-20 16:25:00 -0300

    feat: added dist to deploy on github

---
+++ dist/js/543.5f873093.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdev_roadmap"]=self["webpackChunkdev_roadmap"]||[]).push([[543],{3543:function(n,e,a){a.r(e),a.d(e,{default:function(){return i}});var t=a(266),l=a(4716),r=a(1713),u=function(){var n=this,e=n._self._c;return e(r.Z,[e(t.Z,{staticClass:"d-flex flex-column align-center",attrs:{cols:"12"}},[e(l.Z,{attrs:{"x-large":"",color:"warning"}},[n._v(" mdi-alert ")]),e("p",[n._v("Page not found!")])],1)],1)},o=[],s={name:"pageNotFound"},c=s,d=a(1001),f=(0,d.Z)(c,u,o,!1,null,null,null),i=f.exports}}]);
+//# sourceMappingURL=543.5f873093.js.map
---
+++ dist/js/668.ae08f8bc.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdev_roadmap"]=self["webpackChunkdev_roadmap"]||[]).push([[668],{9668:function(t,n,e){e.r(n),e.d(n,{default:function(){return s}});var u=function(){var t=this,n=t._self._c;return n("button",[t._t("icon"),n("span",[t._v(t._s(t.title))])],2)},r=[],a={name:"navigationButton",props:{title:{type:String,required:!0,default:()=>""}}},i=a,l=e(1001),o=(0,l.Z)(i,u,r,!1,null,"c5543480",null),s=o.exports}}]);
+//# sourceMappingURL=668.ae08f8bc.js.map
